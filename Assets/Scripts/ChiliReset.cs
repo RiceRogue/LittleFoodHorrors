a363eb47bb3345e3ae0f7ee7da5ad792a908ee76
Eric Ni <en1116@nyu.edu>
2021-05-09 23:51:35 -0400

    Chili Pepper Enemy
    Chili Pepper Enemy fully functional, will go to the ingredient shelf and purposefully grab objects and drop them on top of the kiwi monster directly

--- Assets/Scripts/ChiliReset.cs
+++ Assets/Scripts/ChiliReset.cs
@@ -1,19 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class ChiliReset : MonoBehaviour
 {
     public GameObject[] ingredients;
+    public GameObject target;
+    public GameObject ingredient;
+    public float timer;
+    public float moveSpeed;
+    public Vector3 originalPosition;
+    public Quaternion originalRotation;
+
+
+    public bool reset;
+    public bool stealing;
+    public bool traveling;
+
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0;
         ingredients = GameObject.FindGameObjectsWithTag("Ingredients");
+        moveSpeed = 1.2f;
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+        reset = false;
+        stealing = false;
+        traveling = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        transform.rotation = originalRotation;
+
+        if (timer > 30)
+        {
+            ingredient = ingredients[Random.Range(0, ingredients.Length)];
+            reset = false;
+            stealing = true;
+            timer = 0;
+        }
+
         
+
+        //Resets its position to original
+        if (reset == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * moveSpeed);
+
+            if(Vector3.Distance(transform.position, originalPosition) < 2)
+            {
+                reset = false;
+            }
+        }
+
+        //Player wil not be allowed to take ingredients from the Chili pepper!
+        if(traveling == true)
+        {
+            reset = false;
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * moveSpeed);
+            ingredient.transform.position = transform.position + new Vector3(0, -1, 0);
+        }
     }
+
+
+    private void FixedUpdate()
+    {
+        //Stealing the ingredients
+        if (stealing == true)
+        {            
+            transform.position = Vector3.Lerp(transform.position, ingredient.transform.position, Time.deltaTime * moveSpeed);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == ingredient.name)
+        {
+            traveling = true;
+            stealing = false;
+            timer = 0;
+        }
+
+
+        //Touches the end goal
+        if (collision.gameObject.layer == 9)
+        {
+            timer = 0;
+            reset = true;
+            traveling = false;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        //If ingredient leaves the chili, chili resets to original posiiton.
+        if (collision.gameObject.name == ingredient.name)
+        {
+            reset = true;
+            stealing = false;
+        }
+    }
+
+
+
 }
